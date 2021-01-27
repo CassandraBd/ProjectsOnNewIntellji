db29f820d7ae2c6a74aa2f37a2f424c423dd14ba
CassandraBd <bodencassandra@gmail.com>
2021-01-26 21:25:14 -0600

    Initial commit on project

---
+++ src/Practice_Day1/FirstTestClass.java
@@ -0,0 +1,9 @@
+package Practice_Day1;
+
+public class FirstTestClass {
+    public static void main(String[] args) {
+
+        System.out.println("Hello new IntelliJ");
+        System.out.println("Hello world");
+    }
+}
